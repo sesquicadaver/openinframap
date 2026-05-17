07e43daf076926b83af8942f375539c3a47ae925
sesquicadaver <sesquicadaver@gmail.com>
2026-05-17 20:52:05 +0300

    Refactor air trajectory controls into separate toggle button and settings panel components, split into two IControl instances for independent positioning

--- web/src/openinframap.ts
+++ web/src/openinframap.ts
@@ -19,7 +19,7 @@ import { VoltageFilter } from './voltage-filter.ts'
 import { AnnotationEditor } from './annotation-editor.ts'
 import { CacheWarmer } from './cache_warmer.ts'
 import { LanguageSwitcher } from './lang-switcher.ts'
-import { AirTrajectoryController } from './weather/airTrajectoryController.ts'
+import { createAirTrajectoryControls } from './weather/airTrajectoryController.ts'
 
 export default class OpenInfraMap {
   map?: maplibregl.Map
@@ -140,7 +140,9 @@ export default class OpenInfraMap {
 
     map.addControl(new maplibregl.ScaleControl({}), 'bottom-left')
     map.addControl(new CacheWarmer(), 'bottom-right')
-    map.addControl(new AirTrajectoryController(), 'bottom-right')
+    const [windToggle, windPanel] = createAirTrajectoryControls()
+    map.addControl(windToggle, 'bottom-right')
+    map.addControl(windPanel, 'bottom-right')
 
     map.addControl(new KeyControl(), 'top-right')
     map.addControl(layer_switcher, 'top-right')
